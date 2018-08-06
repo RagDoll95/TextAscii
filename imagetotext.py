197551e34732b8e729cda6245bd84a67f154fb9f
RagDoll95 <raghuveergummadi9@gmail.com>
2018-08-05 20:51:17 -0400

    additional changes

--- imagetotext.py
+++ imagetotext.py
@@ -1,7 +1,30 @@
 from PIL import Image
+import pdb
 import sys
+
 
 def processImagePath(path):
     im = Image.open(path)
-    im.rotate(45).show()
+    im.load()
+    (width, height) = im.size
     print(path)
+    x = list(im.getdata())
+    y = []
+    for line in x:
+        y.append(rgbToBrightness(line))
+    pdb.set_trace()
+    return y
+
+
+def rgbToBrightness(rgbband):
+    (R, G, B) = rgbband
+    Y = (R+R+B+G+G+G)/6
+    return Y
+
+
+if __name__ == '__main__':
+    outfile = open("test.txt", "w")
+    y = processImagePath("/home/rag/Pictures/New folder/_7B2zqw33q7fhb7znvtIXcbsmITo09KWg4fFQUIaEFA.jpg")
+    for line in y:
+        print(line, file=outfile)
+    outfile.close()
