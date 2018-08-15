a762d5fdadfbd19362d59d8792463c1f79611ff4
RagDoll95 <anguspepperfinch@gmail.com>
2018-08-14 23:47:56 -0500

    even more trash

--- imagetotext.py
+++ imagetotext.py
@@ -6,15 +6,25 @@ import sys
 def processImagePath(path):
     im = Image.open(path)
     im.load()
+    im.show()
     (width, height) = im.size
     print(path)
     x = list(im.getdata())
     y = []
     for line in x:
         y.append(rgbToBrightness(line))
+    rm = im.resize(newImageSize(width, height))
+    rm.open()
+    rm.show()
     n = width
     final = [y[i * n:(i + 1) * n] for i in range((len(y) + n - 1) // n )]
     return final
+
+
+def newImageSize(width, height):
+    newWidth = 173
+    newHeight = (height * width) / newWidth
+    return (newWidth, int(newHeight))
 
 
 def rgbToBrightness(rgbband):
